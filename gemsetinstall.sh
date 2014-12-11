7e43a7741c358e16d459bea239a5760079281fc1
JP Quicksall <john.paul.quicksall@gmail.com>
2014-12-11 14:50:44 -0800

    Added the base script. Uses the most assumptions.

---
+++ gemsetinstall.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+# Author: JP Quicksall
+# Date Created: 12/10/14
+# Prerequisties
+#  - rvm is installed and sourced as a function
+#  - ruby 2.1.5 is installed
+#  - gem set has not been created yet
+rvm use ruby-2.1.5@officedashboardset --create
+
+exit
