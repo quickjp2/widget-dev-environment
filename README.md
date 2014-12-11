ebced57c1a2e7c6a88ab333e384008eef40a2434
JP Quicksall <john.paul.quicksall@gmail.com>
2014-12-11 15:21:57 -0800

    Finshed Gemset creation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 The project is a series of scripts that assist in setting up a ruby dev environment for developing dashing widgets and dashboards.
+
+Version 1:
+Scripts used: geminstall.sh
+
+Assumptions:
+OS is Ubuntu 14.04 32bit on a virtual machine with 1GB of ram and 20GB of disk
+RVM is already installed
+Script is run inside the local git directory
+A gemset is not pre-created
--- gemsetinstall.sh
+++ gemsetinstall.sh
@@ -6,6 +6,12 @@
 #  - rvm is installed and sourced as a function
 #  - ruby 2.1.5 is installed
 #  - gem set has not been created yet
-rvm use ruby-2.1.5@officedashboardset --create
+
+# Creat the rvm getset to use in the directory
+rvm use ruby-2.1.5@dashingwidgetdev --create
+
+# Create gemset files in directory
+echo "dashingwidgetdev" >> .ruby-gemset
+echo "ruby-2.1.5" >> .ruby-version
 
 exit
